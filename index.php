63bcc88fc7d563c7aaf60f4a2d9c50db9336c73b
ADN830 <73948983+ADN830@users.noreply.github.com>
2021-11-19 19:58:24 -0500

    modificacion de titulo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html style="font-size: 16px;" lang="es-CO">
 <html>
 <head>
-	<title>Seccion de preguntas</title>
+	<title>prueba git</title>
 	 <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
